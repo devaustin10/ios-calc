721b2bbab71c5053f894e0682e0427e8be1474cf
Austin Kim <austin.kim@fiserv.com>
2023-09-01 12:35:00 -0400

    css finished, js buttons not working

---
+++ index.js
@@ -0,0 +1,26 @@
+// will select all HTML elements in the document that have the class "numbers" and return them as a NodeList (a collection of DOM elements)
+const numbers = document.querySelectorAll(".numbers");
+const result = document.querySelectorAll(".result span");
+
+let firstValue = "";
+let isFirstValue = false;
+let secondValue = "";
+let isSecondValue = false;
+let sign = "";
+let resultValue = 0;
+
+for (let i = 0; i < numbers.length; i++) {
+  numbers[i].addEventListener("click", (e) => {
+    let atr = e.target.getAttribute("value");
+    if (isFirstValue === false) {
+      getFirstValue(atr);
+    }
+  });
+}
+
+function getFirstValue(el) {
+  result.innerHTML = "";
+  firstValue += el;
+  result.innerHTML = firstValue;
+  firstValue = +firstValue;
+}
